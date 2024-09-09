a3ce88702118045d03fd2eb0f125afebc7e9aa59
dipansu1196 <166242154+dipansu1196@users.noreply.github.com>
2024-09-09 18:28:45 +0530

    Update rotatearray.cpp

--- rotatearray.cpp
+++ rotatearray.cpp
@@ -24,3 +24,40 @@ for(int i=0;i<n;i++){
 }
     return 0;
 }
+// approach 2
+#include <iostream>
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+vector<int> reverse(vector<int> &arr ,int s,int end){
+    int i=s;
+    int j=end;
+    while(i<j){
+        swap(arr[i],arr[j]);
+        i++;
+        j--;
+    }
+    return arr;
+}
+
+int main() {
+   int n;
+    
+    cin>>n;
+    int k;
+    cin>>k;
+    vector<int> arr(n);
+  //  vector<int> result(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    //k=k%n;
+    reverse(arr,0,n-1);
+    reverse(arr,0,k-1);
+    reverse(arr,k,n-1);
+    
+    for(int i=0;i<n;i++){
+        cout<<arr[i];
+    }
+    return 0;
+}
